8345f8cc50b7b63c9214e9194f7e4e5debc93555
beerpoldet <poladet.a@gmail.com>
2023-03-01 14:18:55 +0700

    Add knex

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const Knex = require("knex");
 
-app.get('/messages', (req, res) => {
-  res.json([])
-})
+const app = express();
+
+const port = 3000;
+const knex = Knex({
+  client: "mysql2",
+  connection: {
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "my-secret-pw",
+    database: "messenger",
+  },
+});
+
+app.get("/messages", async (req, res) => {
+  const result = await knex.raw("select * from users where users.id = ?", [1]);
+  const users = result[0]
+  res.json(users);
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
